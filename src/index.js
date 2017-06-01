2a6a60a784907d019c9131eb63537f2a16239d4d
hylee <hylee@nkia.co.kr>
2017-06-01 14:50:46 +0900

    added AntDesign

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
+import { LocaleProvider } from 'antd';
+import koKR from 'antd/lib/locale-provider/ko_KR';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+    <LocaleProvider locale={koKR}>
+        <App />
+    </LocaleProvider>,
+    rootElement);
+
